9e9fde65e297f5f68aad2eef045fb7b50dd354ea
Jose Merino <jomerinog@gmail.com>
2014-08-02 01:53:12 -0400

    Implement getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,41 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
+
+	var walk = function(element){
+		var found = [];
+
+		// we first check if the current element has the class
+		if(element.classList!==undefined){
+			// by iterating through it's classes
+			for(var i = 0; i< element.classList.length ; i++){
+
+				// comparing
+				if(element.classList[i]==className){
+
+					// and adding
+					found.push(element);
+				}
+			}
+		}
+
+		// we then check it's child elements
+		if(element.childNodes.length>0){
+			// by iterating through the childNodes property
+			for(var i = 0; i< element.childNodes.length; i++){
+
+				// , recursively calling the function on every child
+				var childs = walk(element.childNodes[i]);
+
+				// and pushing any found elements
+				for(var j = 0; j<childs.length; j++){
+					found.push(childs[j]);
+				}
+			}
+		}
+
+		// we return the found elements
+		return found;
+	}
+	return walk(document.body);
 };
